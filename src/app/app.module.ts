38a91ef25d5723aff9ede02c62f3ae00b385947b
selvendiranj <selvendiranj@gmail.com>
2017-03-14 00:18:58 +0530

    angular2 day2 another changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './components/app.component';
+import { HeroesComponent } from './components/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail.component';
+import { DashboardComponent } from './components/dashboard.component';
 import { HeroService } from './services/hero.service';
 
 @NgModule({
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+            { path: 'heroes', component: HeroesComponent },
+            { path: 'dashboard', component: DashboardComponent }
+        ])
     ],
     declarations: [
         AppComponent,
-        HeroDetailComponent
+        HeroesComponent,
+        HeroDetailComponent,
+        DashboardComponent
     ],
-    providers: [HeroService],
+    providers: [
+        HeroService
+    ],
     bootstrap: [AppComponent]
 })
 
--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,33 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../models/hero';
-import { HeroService } from '../services/hero.service';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app/views/app.component.html',
-    styleUrls: ['./app/styles/app.component.css']
+    template: `
+        <h1>{{title}}</h1>
+        <nav>
+            <a routerLink="/dashboard">Dashboard</a>
+            <a routerLink="/heroes">Heroes</a>
+        </nav>
+        <router-outlet></router-outlet>
+        `
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent {
     title = 'Tour of Heroes';
-    heroes: Hero[];
-    selectedHero: Hero;
-
-    constructor(private heroService: HeroService) { }
-
-    ngOnInit(): void {
-        this.getHeroes();
-    }
-
-    getHeroes(): void {
-        // tslint:disable-next-line:no-trailing-whitespace
-        this.heroService.getHeroesSlowly().then((heroes) => {
-            // tslint:disable-next-line:no-trailing-whitespace
-            this.heroes = heroes;
-        });
-    }
-
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
 }
