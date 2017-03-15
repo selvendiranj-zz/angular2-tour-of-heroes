0ae08d306729bd2a90554775d5d7df08c308a3b5
selvendiranj <selvendiranj@gmail.com>
2017-03-15 20:19:40 +0530

    hero filter using shared module

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { SharedModule } from '../shared/shared.module';
 import { HeroesRoutingModule } from './heroes-routing.module';
 
 // Imports for loading & configuring the in-memory web api
@@ -12,6 +12,7 @@ import { InMemoryDataService } from './services/in-memory-data.service';
 import { DashboardComponent } from './components/dashboard.component';
 import { HeroDetailComponent } from './components/hero-detail.component';
 import { HeroesComponent } from './components/heroes.component';
+import { HeroFilterComponent } from './components/hero-filter.component';
 import { HeroService } from './services/hero.service';
 import { HeroSearchComponent } from './components/hero-search.component';
 
@@ -21,12 +22,14 @@ import { HeroSearchComponent } from './components/hero-search.component';
         FormsModule,
         HttpModule,
         InMemoryWebApiModule.forRoot(InMemoryDataService),
-        HeroesRoutingModule
+        HeroesRoutingModule,
+        SharedModule
     ],
     declarations: [
         DashboardComponent,
         HeroDetailComponent,
         HeroesComponent,
+        HeroFilterComponent,
         HeroSearchComponent
     ],
     providers: [
